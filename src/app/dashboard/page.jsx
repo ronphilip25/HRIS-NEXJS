19830b32fd1436efaa24d3e99d83bc0593ab6878
ronlip2571 <sanchezronphilip@gmail.com>
2024-09-11 11:06:21 +0800

    Refactored Status Card

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -9,8 +9,8 @@ import CompanyNews from '../components/CompanyNews';
 import MainLayout from '../Layout/MainLayout';
 import { useState, useEffect } from 'react';
 import Loading from '../dashboard/loading';
-import DashboardWidget from '../components/DashboardWidget';
 import TaskList from '../components/TaskList';
+import StatusCard from '../components/StatusCard';
 
 
 const Dashboard = () => {
@@ -34,7 +34,24 @@ const Dashboard = () => {
         {isLoading ? <Loading /> :
           <div className='container mx-auto px-4 lg:h-auto md:overflow-x-auto md:h-screen overflow-x-scroll h-screen'>
             <div className='lg:grid lg:grid-cols-4 lg:gap-4 lg:max-[1440px]:gap-2 lg:max-[1024px]:gap-2 md:grid md:grid-cols-2 md:gap-2 gap-4 md:flex-shrink-0 flex flex-col md:max-m-auto h-auto lg:max-mx-auto mb-4 md:mb-4'>
-              <DashboardWidget />
+                <StatusCard
+                  color="#D0E9FE"
+                  iconColor="#045CA4"
+                  title="Attended"
+                  count={25}
+                />
+                <StatusCard
+                  color="#F0E1F1"
+                  iconColor="#AC5BB3"
+                  title="On Leave"
+                  count={3}
+                />
+                <StatusCard
+                  color="#FAD2D2"
+                  iconColor="#E94545"
+                  title="Absent"
+                  count={1}
+                />
               <CompanyNews />
               <div className="lg:col-span-2 md:col-span-2 ">
                 <div className="grid grid-cols-1 md:flex-shrink-0">
