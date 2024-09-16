ddae0a4f9f81d61624316ff714ad627b96e92784
ronlip2571 <sanchezronphilip@gmail.com>
2024-09-16 12:27:00 +0800

    added buttons

--- src/app/attendance/monthly/page.jsx
+++ src/app/attendance/monthly/page.jsx
@@ -17,7 +17,7 @@ const page = () => {
               <div className="flex items-center lg:justify-end lg:max-[1440px]:w-[75%]">
                   <div className="flex justify-center lg:mt-16 border relative bg-blue-700 text-white rounded-lg lg:w-36 lg:h-11 w-[100%] h-12">
                     <button className='  text-white text-xs font-sans font-inter font-bold'>
-                      Download Excel
+                     <a href="/not-found">Download Excel</a>
                     </button>
                   </div>
               </div>
