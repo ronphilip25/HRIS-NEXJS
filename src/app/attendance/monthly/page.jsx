79a217e43ee93b4d725ba53ffe19d8f479c85820
ronlip2571 <sanchezronphilip@gmail.com>
2024-09-16 11:08:04 +0800

    adjusted the buttons in monthly page

--- src/app/attendance/monthly/page.jsx
+++ src/app/attendance/monthly/page.jsx
@@ -13,11 +13,11 @@ const page = () => {
         <div className='container mx-auto px-4 '>
           <div className='lg:grid lg:grid-cols-4 lg:gap-4 lg:max-[1440px]:gap-2 lg:max-[1024px]:gap-2 md:grid md:grid-cols-2 md:gap-2 gap-4 md:flex-shrink-0 flex flex-col md:max-m-auto h-auto lg:max-mx-auto  mb-4 md:mb-4'>
             <AttendanceWidget />
-            <div className="col-start-4">
-              <div className="flex items-center justify-end lg:max-[1440px]:w-[75%]">
+            <div className="col-start-4 lg:block md:hidden">
+              <div className="flex items-center lg:justify-end lg:max-[1440px]:w-[75%]">
                 <div className="text-white text-xs font-sans font-inter font-bold">
-                  <div className="flex justify-center mt-16 border relative bg-blue-700 text-white rounded-lg">
-                    <button className=' w-36 h-11 '>
+                  <div className="flex justify-center lg:mt-16 border relative bg-blue-700 text-white rounded-lg">
+                    <button className=' lg:w-36 lg:h-11 w-[400px] h-12'>
                       Download Excel
                     </button>
                   </div>
