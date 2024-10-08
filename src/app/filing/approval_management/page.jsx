802112bbd4584bd37a246e7e8fa712564d1e9eac
ronlip2571 <sanchezronphilip@gmail.com>
2024-10-08 09:59:21 +0800

    added new folders

---
+++ src/app/filing/approval_management/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Approval Management
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/filing/filing_settings/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        Filing Settings
+    </div>
+  )
+}
+
+export default page
