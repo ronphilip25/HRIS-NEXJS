6c31a4c98f17a268d431c8c0b5a45b65b99c1f82
ronlip2571 <sanchezronphilip@gmail.com>
2024-10-12 11:33:21 +0800

    added login page

--- src/app/(auth)/login.js
+++ src/app/(auth)/login.js
@@ -2,9 +2,58 @@ import React from 'react'
 
 const login = () => {
   return (
-    <div>
-      login
-    </div>
+    <>
+
+      <div className="container mx-auto md:max-w-4xl lg:max-w-6xl lg:max-[1024px]:h-auto ">
+
+        <div className="flex justify-center items-center h-screen">
+          <div className="w-full md:w-1/3 lg:w-1/3">
+            <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                  Username
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="username"
+                  type="text"
+                  placeholder="Username"
+                />
+              </div>
+              <div className="mb-6">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="******************"
+                />
+                <p className="text-red-500 text-xs italic">Please enter your password.</p>
+              </div>
+              <div className="flex items-center justify-between">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="button"
+                >
+                  Login
+                </button>
+                <a
+                  className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                  href="#"
+                >
+                  Forgot Password?
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </>
   )
 }
 
